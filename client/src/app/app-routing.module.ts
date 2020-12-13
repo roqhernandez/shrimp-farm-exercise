6061466601aa28681cd7a14b808239045dc1bf84
roqhernandez <roqhernandez@gmail.com>
2020-12-13 18:08:51 -0500

    Add files via upload

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {FarmsListComponent} from './components/farms-list/farms-list.component';
+import {FarmsFormComponent} from './components/farms-form/farms-form.component';
+
+//import {FarmsListComponent} from './components/farms-list/farms-list.component';
+import {PondsFormComponent} from './components/ponds-form/ponds-form.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/farms',
+    pathMatch: 'full'
+  },
+  {
+    path: 'farms',
+    component: FarmsListComponent
+  },
+  {
+    path: 'farms/new',
+    component: FarmsFormComponent
+  },
+  {
+    path: 'farms/edit/:id',
+    component: FarmsFormComponent
+  },
+  {
+    path: 'ponds/new/:farm_id',
+    component: PondsFormComponent
+  },
+  {
+    path: 'ponds/edit/:farm_id/:pond_id',
+    component: PondsFormComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
