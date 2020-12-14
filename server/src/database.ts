df8a4f7d79bbe1ebc12eafaa0aaa39c2a37943c7
roqhernandez <roqhernandez@gmail.com>
2020-12-13 23:31:06 -0500

    Update database.ts

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,4 +1,7 @@
+//Thsi file contains the couple of settings for the project's Database
+
 const mongojs = require('mongojs');
+//Name of the Database: cargill   name of the collection: farms
 const db = mongojs('cargill', ['farms']);
 
 export default db;
